ec109958e558a8d156bfacabdd48bfcfb465558d
yepolonais <58600334+yepolonais@users.noreply.github.com>
2020-05-18 16:21:19 +0200

    update

--- inc/pagination.php
+++ inc/pagination.php
@@ -2,15 +2,16 @@
 
 if( !function_exists( 'theme_pagination' ) ) {
 
-    function theme_pagination($parametre) {
+  function theme_pagination() {
 
+	global $wp_query;
 	global $wp_rewrite;
-	$parametre->query_vars['paged'] > 1 ? $current = $parametre->query_vars['paged'] : $current = 1;
+	$wp_query->query_vars['paged'] > 1 ? $current = $wp_query->query_vars['paged'] : $current = 1;
 
 	$pagination = array(
 		'base' => add_query_arg('page','%#%'),
 		'format' => '',
-		'total' => $parametre->max_num_pages,
+		'total' => $wp_query->max_num_pages,
 		'current' => $current,
     'show_all' => false,
     'end_size'     => 1,
@@ -23,7 +24,7 @@ if( !function_exists( 'theme_pagination' ) ) {
 	if( $wp_rewrite->using_permalinks() )
 		$pagination['base'] = user_trailingslashit( trailingslashit( remove_query_arg( 's', get_pagenum_link( 1 ) ) ) . 'page/%#%/', 'paged' );
 
-	if( !empty($parametre->query_vars['s']) )
+	if( !empty($wp_query->query_vars['s']) )
 		$pagination['add_args'] = array( 's' => str_replace( ' ' , '+', get_query_var( 's' ) ) );
 
 	echo str_replace('page/1/','', paginate_links( $pagination ) );
